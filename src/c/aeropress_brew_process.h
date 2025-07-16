95038a30661d3419b036a3cca4e19691858ae8ca
Chi Hong Edmund Chan <edmundchan@Chis-MacBook-Air.local>
2025-07-16 00:19:02 -0700

    working code for aeropress and french press

--- src/c/aeropress_brew_process.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include <pebble.h>
-
-// Public function to start the Aeropress brewing process
-void aeropress_brew_process_start();
-
-// Functions to destroy resources if they're persistent
-void aeropress_brew_process_deinit();
